4fbbc8db85fa7c20358a0b13483e2c4c37f3c85f
laniakita <101172492+laniakita@users.noreply.github.com>
2024-06-03 18:10:07 +0000

    fix lua_ls

--- lua/main/lazy/nvim-cmp.lua
+++ lua/main/lazy/nvim-cmp.lua
@@ -87,10 +87,10 @@ return {
             workspace = {
               checkThirdParty = false,
               library = {
-                vim.env.VIMRUNTIME
+                vim.env.VIMRUNTIME,
                 -- Depending on the usage, you might want to add additional paths here.
-                -- "${3rd}/luv/library"
-                -- "${3rd}/busted/library",
+                "${3rd}/luv/library",
+                "${3rd}/busted/library",
               }
               -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
               -- library = vim.api.nvim_get_runtime_file("", true)
